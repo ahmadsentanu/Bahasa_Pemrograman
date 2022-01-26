8cc72cc3d2c73d0a90f009952826078e49b33200
root <root@DESKTOP-CII1JDH.localdomain>
2022-01-27 03:22:30 +0700

    fresh from oven

---
+++ UAS/source1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string nama;
+    string nim;
+    
+    cout << "Masukan Nama : ";
+    getline(cin, nama);
+    cout << "Masukan NIM : ";
+    cin>>nim;
+    
+    cout << "Nama : " << nama << endl;
+    cout << "Alamat Nama : " << &nama << endl;
+    cout << "NIm : " << nim << endl;
+    cout << "Alamat Nim : " << &nim << endl;
+    
+    return 0;
+}
---
+++ UAS/source2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void nama()
+{
+    string nama;
+    cout << "Masukan Nama : ";
+    getline(cin, nama);
+}
+void nim()
+{
+    string nim;
+    cout << "Masukan NIM : ";
+    cin>>nim;
+}
+int main()
+{
+    nama();
+    nim();
+    
+    return 0;
+}
